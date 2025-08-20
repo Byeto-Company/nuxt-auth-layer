98299f083856663b50265d06876399c20fe6d47d
marzban-dev <marzban.dev@gmail.com>
2025-08-20 20:06:23 +0330

    Updated

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath } from "node:url";
 
 export default defineNuxtConfig({
     extends: [".."],
+    modules: ["@nuxt/eslint"],
     eslint: {
         config: {
             // Use the generated ESLint config for lint root project as well
