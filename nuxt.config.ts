f0b1585e114671aa1620f3314eb14c1f413b5c6b
marzban-dev <marzban.dev@gmail.com>
2025-09-07 21:01:35 +0330

    Update css build scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,5 @@ import { dirname, resolve } from "pathe";
 const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineNuxtConfig({
-    css: [resolve(currentDir, "app/assets/dist.css")],
+    css: [resolve(currentDir, "./app/assets/dist.css")],
 });
