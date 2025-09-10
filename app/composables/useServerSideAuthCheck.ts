2924c91309f0f4a173638779df3194013ea8eca2
marzban-dev <marzban.dev@gmail.com>
2025-09-10 15:11:38 +0330

    Add wrapper for auth layer

--- app/composables/useServerSideAuthCheck.ts
+++ app/composables/useServerSideAuthCheck.ts
@@ -5,17 +5,21 @@
  * - If valid, prefetches the account data using `suspense`
  * - Runs during server-side rendering via `onServerPrefetch`
  *
+ * @param {boolean} [enabled] - Whether the authentication check should be performed (default: true).
  * @returns {void}
  * @module composables/useServerSideAuthCheck
  */
-const useServerSideAuthCheck = () => {
+
+const useServerSideAuthCheck = (enabled = true) => {
     const { checkAuth } = useAuth();
     const { suspense } = useGetAccount();
 
     onServerPrefetch(async () => {
-        const isValid = await checkAuth();
-        if (isValid) {
-            await suspense();
+        if (enabled) {
+            const isValid = await checkAuth();
+            if (isValid) {
+                await suspense();
+            }
         }
     });
 };
